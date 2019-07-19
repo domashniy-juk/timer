9e64908a94c5766ab6fdac5ff4474cab151f2d68
animeshnik <38006772+animeshny-overlord@users.noreply.github.com>
2019-07-19 20:43:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+  let [count,setCount] = useState(0)
+  let [timer, setTimer] = useState(0.5)
+ 
+
+  const start = () => {
+    if(timer && !count){
+      setCount(count++)
+      setTimer(setInterval(() => setCount(count++), 1000))
+    }
+  }
+  const change = () => {
+    if(timer && count){
+      clearInterval(timer)
+      setTimer(0)
+    }
+    else if(count && !timer ){
+      setCount(count++)
+      setTimer(setInterval(() => setCount(count++), 1000))
+    }
+  }
+  const remove = () => {
+    setCount(0)
+    clearInterval(timer)
+    setTimer(0.5)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className = 'circle'>
+        <div className = 'arrow' style = {{transform: `rotate(${(count-15)*6}deg)`}}/>
+      </div>
+      <div className = 'count'>счетчик: {count}</div>
+      <div>
+        <button onClick = {start}>Старт</button>
+        {timer ? <button onClick = {change}>Стоп</button> :
+        <button onClick = {change}>Продолжить</button>} 
+        <button onClick = {remove}>Сброс</button>
+      </div>
+    </main>
+  )
 }
 
 export default App;

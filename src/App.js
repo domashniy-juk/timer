7449335eb939ea0d2acd7a4a16353f3c729ccc35
unknown <goshan7890@yandex.ru>
2019-07-23 19:52:52 +0300

    prettier add

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
-
-
 const App = () => {
-  let [count,setCount] = useState(0)
-  let [timer, setTimer] = useState(0.5)
- 
-  const start = () => {
-    if(timer && !count){
-      setCount(count++)
-      setTimer(setInterval(() => setCount(count++), 1000))
-    }
-  }
+	let [count, setCount] = useState(0);
+    let [timer, setTimer] = useState(0.5);
+    
+    const inc = () => setCount(count++)
 
-  const change = () => {
-    if(timer && count){
-      clearInterval(timer)
-      setTimer(0)
-    }
-    else if(count && !timer ){
-      setCount(count++)
-      setTimer(setInterval(() => setCount(count++), 1000))
-    }
-  }
+	const start = () => {
+		if (timer && !count) {
+			inc();
+			setTimer(setInterval(() => inc(), 1000));
+		}
+	};
 
-  const remove = () => {
-    setCount(0)
-    clearInterval(timer)
-    setTimer(0.5)
-  }
+	const change = () => {
+		if (timer && count) {
+			clearInterval(timer);
+			setTimer(0);
+		} else if (count && !timer) {
+			inc();
+			setTimer(setInterval(() => inc(), 1000));
+		}
+	};
 
-  useEffect(() => () => clearInterval(timer),[])
+	const remove = () => {
+		setCount(0);
+		clearInterval(timer);
+		setTimer(0.5);
+	};
 
-  return (
-    <main>
-      <div className = 'circle'>
-        <div className = 'arrow' style = {{transform: `rotate(${(count-15)*6}deg)`}}/>
-      </div>
-      <div className = 'count'>счетчик: {count}</div>
-      <div>
-        <button onClick = {start}>Старт</button>
-        {timer ? <button onClick = {change}>Стоп</button> :
-        <button onClick = {change}>Продолжить</button>} 
-        <button onClick = {remove}>Сброс</button>
-      </div>
-    </main>
-  )
-}
+	useEffect(() => () => clearInterval(timer), []);
+
+	return (
+		<div className="main">
+			<div className="main-circle">
+				<div
+					className="main-circle-arrow"
+					style={{ transform: `rotate(${(count - 15) * 6}deg)` }}
+				/>
+			</div>
+			<div className="main-count">счетчик: {count}</div>
+			<div>
+				<button onClick={start}>Старт</button>
+				{timer ? (
+					<button onClick={change}>Стоп</button>
+				) : (
+					<button onClick={change}>Продолжить</button>
+				)}
+				<button onClick={remove}>Сброс</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
